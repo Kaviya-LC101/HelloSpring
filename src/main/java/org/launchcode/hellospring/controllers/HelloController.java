85825eeb6bf2f82fc1cc34d85929e0463735c61f
kaviya <kaviya0925@gmail.com>
2021-10-12 00:19:38 -0500

    ControllerAndExercise

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -22,13 +22,11 @@ public class HelloController {
         return "Goodbye,Spring!";
     }
 
-    //Handles request of the form /hello?name=LaunchCode
-
-    @RequestMapping(method={RequestMethod.GET,RequestMethod.POST})
-
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello, "+name+"!";
-    }
+    //Handles request of the form /hello?name=LaunchCode@RequestMapping(method={RequestMethod.GET,RequestMethod.POST})
+//
+//    public String helloWithQueryParam(@RequestParam String name){
+//        return "Hello, "+name+"!";
+//    }
     // Handles request of the form /hello/LaunchCode
     @GetMapping("{name}")
 
@@ -41,13 +39,49 @@ public class HelloController {
     public String helloForm(){
         return "<html>" +
                 "<body>" +
-                "<form action='hello' method='post'>" +
+                "<form action='/hello' method='post'>" +//submit a request to /hello
                 "<input type='text' name='name'>" +
+                "<select name='language'><br>" +
+                "<option value='English'>English</option>" +
+                "<option value='French'>French</option>"+
+                "<option value='Italian'>Italian</option>"+
+                "<option value='Spanish'>Spanish</option>"+
+                "<option value='German'>German</option>"+
                 "<input type='submit' value='Greet me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
+    @RequestMapping(method = RequestMethod.POST)
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
 
+
+
+        return createMessage(name, language);
+
+
+    }
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+
+
+        if (l.equals("English")) {
+            greeting = "Hello";
+        }
+        else if (l.equals("French")) {
+            greeting = "Bonjour";
+        }
+        else if (l.equals("Italian")) {
+            greeting = "Bonjourno";
+        }
+        else if (l.equals("Spanish")) {
+            greeting = "Hola";
+        }
+        else if (l.equals("German")) {
+            greeting = "Hallo";
+        }
+
+        return greeting + " " + n;
+    }
 
 }
